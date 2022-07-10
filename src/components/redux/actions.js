95507506db142b0d722f94ecc24c00b504edfd64
Oleksandr Soloviov <shakal194@gmail.com>
2022-07-10 22:49:38 +0300

    upd

--- src/components/redux/actions.js
+++ src/components/redux/actions.js
@@ -4,4 +4,5 @@ const changeFilter = createAction('contact/change_filter');
 const contactAdd = createAction('contact/add');
 const contactDelete = createAction('contact/delete');
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default { changeFilter, contactAdd, contactDelete };
